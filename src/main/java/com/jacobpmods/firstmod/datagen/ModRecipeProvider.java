89f9939ce4b7f777fce8fe1d1731e41b4eea0aa7
JacobProut <Jprout@wgu.edu>
2024-10-03 17:17:57 -0500

    Ore/Smelting fix

--- src/main/java/com/jacobpmods/firstmod/datagen/ModRecipeProvider.java
+++ src/main/java/com/jacobpmods/firstmod/datagen/ModRecipeProvider.java
@@ -1,5 +1,6 @@
 package com.jacobpmods.firstmod.datagen;
 
+import com.jacobpmods.firstmod.FirstMod;
 import com.jacobpmods.firstmod.block.ModBlocks;
 import com.jacobpmods.firstmod.item.ModItems;
 import net.minecraft.core.HolderLookup;
@@ -8,6 +9,7 @@ import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.MinecartItem;
+import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
@@ -22,10 +24,12 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
     @Override
     protected void buildRecipes(RecipeOutput pRecipeOutput) {
 
-        List<ItemLike> NEXON_SMELTABLES= List.of(ModItems.nexon.get());
+        //Nexon Ore Smelting/Blasting Methods
+        List<ItemLike> NEXON_SMELTABLES= List.of(ModItems.nexon.get()); //Can add other nexon items here that can be smelted.
         oreSmelting(pRecipeOutput, NEXON_SMELTABLES, RecipeCategory.MISC, ModItems.nexonheated.get(), 0.30f, 200, "nexon");
         oreBlasting(pRecipeOutput, NEXON_SMELTABLES, RecipeCategory.MISC, ModItems.nexonheated.get(), 0.30f, 100, "nexon");
 
+        //Nexon Block Recipe
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.NEXON_BLOCK.get(), 1)
                 .pattern("AAA")
                 .pattern("AAA")
@@ -33,10 +37,12 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('A', ModItems.nexoningot.get())
                 .unlockedBy(getHasName(ModItems.nexoningot.get()), has(ModItems.nexoningot.get())).save(pRecipeOutput);
 
+        // 9 Nexon from Block Recipe
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.nexon.get(), 9)
                 .requires(ModBlocks.NEXON_BLOCK.get())
                         .unlockedBy(getHasName(ModBlocks.NEXON_BLOCK.get()), has(ModBlocks.NEXON_BLOCK.get())).save(pRecipeOutput);
 
+        //Nexon ingot Craft Recipe
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.nexoningot.get(), 1)
                 .pattern("HHH")
                 .pattern("HHH")
@@ -44,11 +50,13 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('H', ModItems.nexonheated.get())
                 .unlockedBy(getHasName(ModItems.nexoningot.get()), has(ModItems.nexoningot.get())).save(pRecipeOutput);
 
+        //Nexon Reinforced Ingot Craft Recipe
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.nexonreinforcedingot.get(), 1)
                 .requires(ModItems.nexoningot.get(),1)
                 .requires(Items.NETHERITE_INGOT, 1)
                 .unlockedBy(getHasName(ModItems.nexoningot.get()), has(ModItems.nexoningot.get())).save(pRecipeOutput);
 
+        //Nexon Pickaxe Craft Recipe
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.nexonpickaxe.get(), 1)
                 .pattern("AAA")
                 .pattern(" S ")
@@ -56,9 +64,24 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('A', ModItems.nexonreinforcedingot.get())
                 .define('S', Items.STICK)
                 .unlockedBy(getHasName(ModItems.nexonreinforcedingot.get()), has(ModItems.nexonreinforcedingot.get())).save(pRecipeOutput);
+    }
+    protected static void oreSmelting(RecipeOutput recipeOutput, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult,
+                                      float pExperience, int pCookingTIme, String pGroup) {
+        oreCooking(recipeOutput, RecipeSerializer.SMELTING_RECIPE, SmeltingRecipe::new, pIngredients, pCategory, pResult,
+                pExperience, pCookingTIme, pGroup, "_from_smelting");
+    }
 
+    protected static void oreBlasting(RecipeOutput recipeOutput, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult,
+                                      float pExperience, int pCookingTime, String pGroup) {
+        oreCooking(recipeOutput, RecipeSerializer.BLASTING_RECIPE, BlastingRecipe::new, pIngredients, pCategory, pResult,
+                pExperience, pCookingTime, pGroup, "_from_blasting");
+    }
 
-
-
+    protected static <T extends AbstractCookingRecipe> void oreCooking(RecipeOutput recipeOutput, RecipeSerializer<T> pCookingSerializer, AbstractCookingRecipe.Factory<T> factory,
+                                                                       List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup, String pRecipeName) {
+        for(ItemLike itemlike : pIngredients) {
+            SimpleCookingRecipeBuilder.generic(Ingredient.of(itemlike), pCategory, pResult, pExperience, pCookingTime, pCookingSerializer, factory).group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike))
+                    .save(recipeOutput, FirstMod.MOD_ID + ":" + getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
+        }
     }
 }
