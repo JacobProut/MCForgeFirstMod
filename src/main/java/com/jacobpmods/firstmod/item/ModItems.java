3e4b723faf02eb466b43a41c68227cf6c2d592fe
JacobProut <Jprout@wgu.edu>
2024-10-02 19:32:33 -0500

    First Attempt at modpacks for minecraft java 1.21.1

---
+++ src/main/java/com/jacobpmods/firstmod/item/ModItems.java
@@ -0,0 +1,20 @@
+package com.jacobpmods.firstmod.item;
+
+import com.jacobpmods.firstmod.FirstMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MOD_ID);
+
+    public static final RegistryObject<Item> speedapple = ITEMS.register("speedapple", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> fireball = ITEMS.register("throwablefireball", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
